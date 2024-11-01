728e6f01ba805dd177719ae98fa4f0d5f48b182b
jeresc <jeremiastomassrc@gmail.com>
2024-10-31 23:37:57 -0300

    feat: add two-sum solution

---
+++ array-hashing/two-sum.go
@@ -0,0 +1,14 @@
+package arrayHashing
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for idx, num := range nums {
+
+		if val, found := m[target-num]; found {
+			return []int{val, idx}
+		}
+
+		m[num] = idx
+	}
+	return nil
+}
